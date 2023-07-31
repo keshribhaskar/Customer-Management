2839b249ae7b1bc4b9def6a30af87dfb38ed9e3c
unknown <2022mt93183@wilp.bits-pilani.ac.in>
2023-07-31 22:57:24 +0530

    project added

---
+++ src/test/java/com/assignment/ekart/custms/CustmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.ekart.custms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
